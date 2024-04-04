24dbaf6d08dece8483ad3c33275cf2c94ab0b9c0
Ryan <30917551+Rldeckard@users.noreply.github.com>
2024-04-03 21:42:13 -0400

    Add files via upload

---
+++ handler.go
@@ -0,0 +1,20 @@
+package closeHandler
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// Setup a listener thorugh a seperate Go Routine to Catch ^C and gracefully shutdown.
+func Init() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\n- Ctrl+C pressed in Terminal. Gracefully shutting down.")
+		os.Exit(1)
+	}()
+	return
+}
